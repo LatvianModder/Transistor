05ace87eafef6feca5c813b1fcd2631da1ac06ff
LatvianModder <latvianmodder@gmail.com>
2018-08-18 04:12:01 +0300

    Removed event subscriber annotation from TransistorItems

--- src/main/java/com/latmod/transistor/TransistorItems.java
+++ src/main/java/com/latmod/transistor/TransistorItems.java
@@ -2,13 +2,11 @@ package com.latmod.transistor;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * @author LatvianModder
  */
-@Mod.EventBusSubscriber(modid = Transistor.MOD_ID)
 @GameRegistry.ObjectHolder(Transistor.MOD_ID)
 public class TransistorItems
 {
